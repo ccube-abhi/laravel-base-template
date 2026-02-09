b2eabe7834a0dd57ec3f69dc2c05554948819c93
rajendranewput <rajendra@newput.com>
2026-02-09 14:22:52 +0530

    fix docker

--- database/migrations/2025_07_03_103332_create_blogs_table.php
+++ database/migrations/2025_07_03_103332_create_blogs_table.php
@@ -10,7 +10,7 @@ return new class () extends Migration {
      */
     public function up(): void
     {
-        Schema::connection('mysql_second')->create('blogs', function (Blueprint $table) {
+        Schema::create('blogs', function (Blueprint $table) {
             $table->id();
             $table->unsignedBigInteger('user_id');
             $table->string('title')->nullable();
